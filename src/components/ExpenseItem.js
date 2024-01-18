9b78b701fc55c86b82b75c7d1e20cb3640291e8b
Akhil Basnotra <akhilbasnotra24@gmail.com>
2024-01-19 00:03:25 +0530

    removed unnecessary buttons

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,25 +1,17 @@
-import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDetails from "./ExpenseDetails";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 
 let ExpenseItem = (props) => {
-  const [expense, setExpense] = useState(props.amount);
-
-  let clickHandler = () => {
-    setExpense("$100");
-    console.log("delete");
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails
-        amount={expense}
+        amount={props.title}
         location={props.location}
         title={props.title}
       />
-      <button onClick={clickHandler}>Change Expense</button>
     </Card>
   );
 };
